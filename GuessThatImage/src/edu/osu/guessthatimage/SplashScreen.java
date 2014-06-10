b663547186e9d02e7fc4d64afaed416ebf1569b3
ssssos <liwenyuan1991@gmail.com>
2014-06-10 13:05:52 -0400

    testing

--- GuessThatImage/src/edu/osu/guessthatimage/SplashScreen.java
+++ GuessThatImage/src/edu/osu/guessthatimage/SplashScreen.java
@@ -14,6 +14,7 @@ import android.os.Build;
 public class SplashScreen extends ActionBarActivity {
 	//Here is a nice and simple test!!!!
 	//Here is another nice and simple test!!!
+	//Here is another another nice and simple test!!!
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
